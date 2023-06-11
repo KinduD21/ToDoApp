915d02ced073581b2fed534592b611541fdbef2d
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-06-11 23:15:47 +0300

    Added open/close sidebar button functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+const hideMenuBtn = document.querySelector("#hide-menu-button");
+const editor = document.querySelector(".editor");
+const sidebar = document.querySelector("aside");
+const sidebarOverlay = document.querySelector(".sidebar-overlay");
+
+hideMenuBtn.addEventListener("click", () => {
+  if (sidebar.classList.contains("isSidebarVisible")) {
+    sidebar.classList.remove("isSidebarVisible");
+    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
+  } else {
+    sidebar.classList.add("isSidebarVisible");
+    sidebarOverlay.classList.add("isSidebarOverlayVisible");
+  }
+});
+
+window.onclick = function (event) {
+  if (
+    event.target == sidebarOverlay &&
+    sidebarOverlay.classList.contains("isSidebarOverlayVisible")
+  ) {
+    sidebar.classList.remove("isSidebarVisible");
+    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
+  }
+};
