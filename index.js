7770035ed099a58fb6f4784f4878a1285f61cf90
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-06-15 00:55:34 +0300

    Continue working on modal windows and JS part

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const body = document.querySelector("body");
 const hideMenuBtn = document.querySelector("#hide-menu-button");
 const editor = document.querySelector(".editor");
 const sidebar = document.querySelector("aside");
 const sidebarOverlay = document.querySelector(".sidebar-overlay");
+const addTaskBtns = document.querySelectorAll("#add-task-button");
+const addTaskModalOverlay = document.querySelector(".add-task-modal-overlay");
+const addTaskModal = document.querySelector(".add-task-modal");
+const cancelBtn = document.querySelector(".cancel-button");
+
+window.onclick = function (event) {
+  if (
+    event.target == sidebarOverlay &&
+    sidebarOverlay.classList.contains("isSidebarOverlayVisible")
+  ) {
+    sidebar.classList.remove("isSidebarVisible");
+    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
+  }
+}; // Дописать функцию для addTaskModalOverlay, проблема с условием if???
 
 hideMenuBtn.addEventListener("click", () => {
   if (sidebar.classList.contains("isSidebarVisible")) {
@@ -13,12 +28,14 @@ hideMenuBtn.addEventListener("click", () => {
   }
 });
 
-window.onclick = function (event) {
-  if (
-    event.target == sidebarOverlay &&
-    sidebarOverlay.classList.contains("isSidebarOverlayVisible")
-  ) {
-    sidebar.classList.remove("isSidebarVisible");
-    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
-  }
-};
+addTaskBtns.forEach((addTaskBtn) => {
+  addTaskBtn.addEventListener("click", () => {
+    addTaskModalOverlay.classList.add("visible");
+    addTaskModal.classList.add("visible");
+  });
+});
+
+cancelBtn.addEventListener("click", () => {
+  addTaskModalOverlay.classList.remove("visible");
+  addTaskModal.classList.remove("visible");
+});
