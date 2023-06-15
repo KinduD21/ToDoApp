4f4fb56279cfbc86963bdfa17b7176abdfcbdb35
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-06-15 18:42:43 +0300

    Working on add project modal

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ const sidebarOverlay = document.querySelector(".sidebar-overlay");
 const addTaskBtns = document.querySelectorAll("#add-task-button");
 const addTaskModalOverlay = document.querySelector(".add-task-modal-overlay");
 const addTaskModal = document.querySelector(".add-task-modal");
-const cancelBtn = document.querySelector(".cancel-button");
+const cancelBtns = document.querySelectorAll(".cancel-button");
+const addProjectBtn = document.querySelector(".add-project-button");
+const addProjectModalOverlay = document.querySelector(
+  ".add-project-modal-overlay"
+);
+const addProjectModal = document.querySelector(".add-project-modal");
 
 window.onclick = function (event) {
   if (
     event.target == sidebarOverlay &&
-    sidebarOverlay.classList.contains("isSidebarOverlayVisible")
+    sidebarOverlay.classList.contains("visible")
   ) {
-    sidebar.classList.remove("isSidebarVisible");
-    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
+    sidebar.classList.remove("visible");
+    sidebarOverlay.classList.remove("visible");
   }
 }; // Дописать функцию для addTaskModalOverlay, проблема с условием if???
 
 hideMenuBtn.addEventListener("click", () => {
-  if (sidebar.classList.contains("isSidebarVisible")) {
-    sidebar.classList.remove("isSidebarVisible");
-    sidebarOverlay.classList.remove("isSidebarOverlayVisible");
+  if (sidebar.classList.contains("visible")) {
+    sidebar.classList.remove("visible");
+    sidebarOverlay.classList.remove("visible");
   } else {
-    sidebar.classList.add("isSidebarVisible");
-    sidebarOverlay.classList.add("isSidebarOverlayVisible");
+    sidebar.classList.add("visible");
+    sidebarOverlay.classList.add("visible");
   }
 });
 
@@ -35,7 +40,16 @@ addTaskBtns.forEach((addTaskBtn) => {
   });
 });
 
-cancelBtn.addEventListener("click", () => {
-  addTaskModalOverlay.classList.remove("visible");
-  addTaskModal.classList.remove("visible");
+addProjectBtn.addEventListener("click", () => {
+  addProjectModalOverlay.classList.add("visible");
+  addProjectModal.classList.add("visible");
+});
+
+cancelBtns.forEach((cancelBtn) => {
+  cancelBtn.addEventListener("click", () => {
+    addTaskModalOverlay.classList.remove("visible");
+    addTaskModal.classList.remove("visible");
+    addProjectModalOverlay.classList.remove("visible");
+    addProjectModal.classList.remove("visible");
+  });
 });
