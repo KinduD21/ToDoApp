b661cd19477b61ddffea9ffb76b3ce1d29385d3d
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-07-13 00:53:05 +0300

    Working on modules creating, projects modules

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,5 @@
+import { deleteIconSvg } from "./deleteIconSvg.js";
+
 const projects = [];
 const editor = document.querySelector(".editor");
 const editorHeading = editor.querySelector("h2");
@@ -18,24 +20,6 @@ const projectsListContainer = document.querySelector(
 const sidebarProjectsArrowIcon = document.querySelector(
   ".sidebar-projects-arrow-icon"
 );
-
-// module modal
-// let open = false
-// export function modalProjects() {
-//     createProject
-// }
-
-// export function modalTasks() {
-//   createProject
-// }
-
-// //module project
-// import { modalProjects } from './modal'
-// modalProject('projects')
-
-// //module tasks
-// import { modaltasks } from './modal'
-// modalTasks('tasks')
 
 sidebarProjectsArrowIcon.addEventListener("click", () => {
   if (sidebarProjectsArrowIcon.classList.contains("expanded")) {
@@ -78,20 +62,7 @@ addProjectInnerBtn.addEventListener("click", () => {
         ></path>
       </svg>
       <span class="project-name">${project.title}</span>
-      <svg class="delete-project-icon">
-        <g fill="none" fill-rule="evenodd">
-          <path d="M0 0h24v24H0z"></path>
-          <rect x="5" y="6" fill="currentColor" rx=".5"></rect>
-          <path
-            fill="currentColor"
-            d="M10 9h1v8h-1V9zm3 0h1v8h-1V9z"
-          ></path>
-          <path
-            stroke="currentColor"
-            d="M17.5 6.5h-11V18A1.5 1.5 0 0 0 8 19.5h8a1.5 1.5 0 0 0 1.5-1.5V6.5zm-9 0h7V5A1.5 1.5 0 0 0 14 3.5h-4A1.5 1.5 0 0 0 8.5 5v1.5z"
-          ></path>
-        </g>
-      </svg>
+      ${deleteIconSvg}
     </button>
   </li>
   `; // import SVG from separate modules
